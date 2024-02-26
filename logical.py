73d333d7acad80543d740222604aabe4f9aa22b2
Tenzin02230110 <02230110.cst@rub.edu.bt>
2024-02-26 14:54:59 +0530

    Second commit logical.py

---
+++ logical.py
@@ -0,0 +1,21 @@
+#and operator
+x = 11
+print(x>5 and x<15)
+
+y = 12
+print(y>10 and y % 5 ==0)
+
+#or operator
+x = 11
+print(x<5 or x>15)
+
+y = 12
+print(y<10 or y % 2==0)
+
+#not operator
+x=5
+print(not(x>3 and x<10))
+
+y=12
+print(not (y>10 and y % 5 ==0))
+
