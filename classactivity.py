ab40ef8e7d38c1faa248531f44a287b57234b60d
Tenzin02230110 <02230110.cst@rub.edu.bt>
2024-05-13 15:51:42 +0600

    Classactivity.py

---
+++ classactivity.py
@@ -0,0 +1,13 @@
+name = input("Enter your good name:")
+num1 = float(input("Enter any number:"))
+num2 = float(input("Enter another number:"))
+addition = num1 +num2
+addition_result = print(addition)
+subtraction = num1 -num2
+subtraction_result = print(subtraction)
+multiplication = num1 * num2
+multiplication_result = print(multiplication)
+division = num1 /num2
+division_result=print(division)
+
+print(f"Hello {name}")
